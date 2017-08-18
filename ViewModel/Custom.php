37757635b8184107ee41ba20ff0c94f18f9c1851
Jisse Reitsma <jisse@yireo.com>
2017-08-18 12:19:30 +0200

    Add empty Custom ViewModel for people customizing things too much

---
+++ ViewModel/Custom.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * GoogleTagManager2 plugin for Magento
+ *
+ * @package     Yireo_GoogleTagManager2
+ * @author      Yireo (https://www.yireo.com/)
+ * @copyright   Copyright 2017 Yireo (https://www.yireo.com/)
+ * @license     Open Source License (OSL v3)
+ */
+
+namespace Yireo\GoogleTagManager2\ViewModel;
+
+/**
+ * Class \Yireo\GoogleTagManager2\ViewModel\Custom
+ */
+class Custom
+{
+}
