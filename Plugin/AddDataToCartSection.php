7ac2ae3cf809a099f92fc7079dba532fc12fa53e
Jisse Reitsma <jisse@yireo.com>
2021-07-10 12:34:28 +0200

    Renamed existing plugin interceptors

---
+++ Plugin/AddDataToCartSection.php
@@ -0,0 +1,185 @@
+<?php declare(strict_types=1);
+/**
+ * GoogleTagManager2 plugin for Magento
+ *
+ * @author      Yireo (https://www.yireo.com/)
+ * @copyright   Copyright 2019 Yireo (https://www.yireo.com/)
+ * @license     Open Source License (OSL v3)
+ */
+
+namespace Yireo\GoogleTagManager2\Plugin;
+
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Checkout\CustomerData\Cart as CustomerData;
+use Magento\Checkout\Model\Cart as CheckoutCart;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Quote\Model\Quote as QuoteModel;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order\Item;
+
+class AddDataToCartSection
+{
+    /**
+     * @var CheckoutSession
+     */
+    private $checkoutSession;
+
+    /**
+     * @var QuoteModel
+     */
+    private $quote;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @param CheckoutSession $checkoutSession
+     * @param CheckoutCart $checkoutCart
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        CheckoutSession $checkoutSession,
+        CheckoutCart $checkoutCart,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->quote = $checkoutCart->getQuote();
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @param CustomerData $subject
+     * @param array $result
+     * @return array
+     */
+    public function afterGetSectionData(CustomerData $subject, $result)
+    {
+        if (empty($result) || !is_array($result)) {
+            return $result;
+        }
+
+        $result['gtm'] = [
+            'transactionEntity' => 'QUOTE',
+            'transactionId' => $this->getQuoteId(),
+            'transactionAffiliation' => $this->getWebsiteName(),
+            'transactionTotal' => $this->getTotalAmount(),
+            'transactionTax' => $this->getTaxAmount(),
+            'transactionCurrency' => $this->getBaseCurrency(),
+            'transactionProducts' => $this->getItemsAsArray()
+        ];
+
+        return $result;
+    }
+
+    /**
+     * @return int
+     */
+    private function getQuoteId()
+    {
+        return $this->quote->getId();
+    }
+
+    /**
+     * @return string
+     */
+    private function getWebsiteName()
+    {
+        return $this->scopeConfig->getValue('general/store_information/name');
+    }
+
+    /**
+     * @return string
+     */
+    private function getBaseCurrency()
+    {
+        return (string) $this->quote->getQuoteCurrencyCode();
+    }
+
+    /**
+     * @return float
+     */
+    private function getTotalAmount()
+    {
+        return (float)$this->quote->getGrandTotal();
+    }
+
+    /**
+     * @return float
+     */
+    private function getTaxAmount()
+    {
+        return (float)$this->quote->getGrandTotal() - $this->quote->getSubtotal();
+    }
+
+    /**
+     * @return OrderInterface
+     */
+    private function getOrder(): OrderInterface
+    {
+        return $this->checkoutSession->getLastRealOrder();
+    }
+
+    /**
+     * @return string
+     */
+    private function getOrderId(): string
+    {
+        if ($this->hasOrder() === false) {
+            return '';
+        }
+
+        return (string) $this->getOrder()->getIncrementId();
+    }
+
+    /**
+     * @return bool
+     */
+    private function hasOrder()
+    {
+        $order = $this->getOrder();
+        if ($order->getItems()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    private function hasQuote()
+    {
+        $quote = $this->quote;
+        if ($quote->getItems()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Return all quote items as array
+     *
+     * @return array
+     */
+    private function getItemsAsArray()
+    {
+        $quote = $this->quote;
+        $data = [];
+
+        foreach ($quote->getItemsCollection() as $item) {
+            /** @var Item $item */
+            $data[] = [
+                'productId' => $item->getProduct()->getId(),
+                'sku' => $item->getProduct()->getSku(),
+                'name' => $item->getProduct()->getName(),
+                'price' => $item->getProduct()->getPrice(),
+                'quantity' => $item->getQty(),
+            ];
+        }
+
+        return $data;
+    }
+}
---
+++ Plugin/AddDataToCustomerSection.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+/**
+ * GoogleTagManager2 plugin for Magento
+ *
+ * @author      Yireo (https://www.yireo.com/)
+ * @copyright   Copyright 2017 Yireo (https://www.yireo.com/)
+ * @license     Open Source License (OSL v3)
+ */
+
+namespace Yireo\GoogleTagManager2\Plugin;
+
+use Magento\Customer\Api\GroupRepositoryInterface;
+use Magento\Customer\CustomerData\Customer as CustomerData;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+class AddDataToCustomerSection
+{
+    /**
+     * @var CustomerSession
+     */
+    private $customerSession;
+
+    /**
+     * @var GroupRepositoryInterface
+     */
+    private $groupRepository;
+
+    /**
+     * Customer constructor.
+     * @param CustomerSession $customerSession
+     * @param GroupRepositoryInterface $groupRepository
+     */
+    public function __construct(
+        CustomerSession $customerSession,
+        GroupRepositoryInterface $groupRepository
+    ) {
+        $this->customerSession = $customerSession;
+        $this->groupRepository = $groupRepository;
+    }
+
+    /**
+     * @param CustomerData $subject
+     * @param $result
+     *
+     * @return mixed
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function afterGetSectionData(CustomerData $subject, $result)
+    {
+        if (empty($result)) {
+            return $result;
+        }
+
+        $customerGroup = $this->groupRepository->getById($this->customerSession->getCustomerGroupId());
+
+        $result['id'] = $this->customerSession->getCustomerId();
+        $result['group_id'] = $customerGroup->getId();
+        $result['group_code'] = $customerGroup->getCode();
+        return $result;
+    }
+}
