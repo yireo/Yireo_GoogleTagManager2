65a15e1d5d7339c49b5f9dde9e1ebc193399d4df
Jisse Reitsma <jisse@yireo.com>
2022-10-29 09:54:25 +0200

    Refactor GitHub Actions

---
+++ .github/workflows/extdn-integration-tests-pre-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+composer config minimum-stability dev
+composer config prefer-stable false
+
+composer require yireo/magento2-integration-test-helper --no-update
+
+composer require yireo/magento2-replace-bundled:^4.0 --no-update
+composer require yireo/magento2-replace-inventory:^4.0 --no-update
+composer require yireo/magento2-replace-pagebuilder:^4.0 --no-update
