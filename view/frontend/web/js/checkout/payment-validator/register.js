9c139b635432a2d1766d12ca8db72c78b95a14a4
jisse Reitsma <jisse@yireo.com>
2022-12-09 11:51:17 +0100

    Backend model for container ID field

--- view/frontend/web/js/checkout/payment-validator/register.js
+++ view/frontend/web/js/checkout/payment-validator/register.js
@@ -2,10 +2,12 @@ define(
     [
         'uiComponent',
         'Magento_Checkout/js/model/payment/additional-validators',
-        'Yireo_GoogleTagManager2/js/checkout/payment-validator/reload-customer-data'
+        'Yireo_GoogleTagManager2/js/checkout/payment-validator/reload-customer-data',
+        'yireoGoogleTagManagerLogger'
     ],
-    function (Component, additionalValidators, reloadCustomerDataValidator) {
+    function (Component, additionalValidators, reloadCustomerDataValidator, logger) {
         'use strict';
+        logg('Registering additional payment validator');
         additionalValidators.registerValidator(reloadCustomerDataValidator);
         return Component.extend({});
     }
