7ffb0d8285ec0b05ae869c9f6ab03a4ec10faa9e
jisse Reitsma <jisse@yireo.com>
2023-03-21 17:27:26 +0100

    Add currency and value to `view_cart` event

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- Reset ecommerce `dataLayer` everywhere
+- Use `window.dataLayer` instead of `dataLayer`
 
 ## [3.1.0] - 19 March 2023
 ### Fixed
---
+++ DataLayer/Tag/Cart/CartValue.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Yireo\GoogleTagManager2\DataLayer\Tag\Cart;
+
+use Magento\Checkout\Model\Cart as CartModel;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Yireo\GoogleTagManager2\Api\Data\TagInterface;
+
+class CartValue implements TagInterface
+{
+    private CartModel $cartModel;
+
+    /**
+     * @param CartModel $cartModel
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     */
+    public function __construct(
+        CartModel $cartModel
+    ) {
+        $this->cartModel = $cartModel;
+    }
+
+    /**
+     * @return array
+     * @throws NoSuchEntityException
+     */
+    public function get(): string
+    {
+        return (string)$this->cartModel->getQuote()->getBaseGrandTotal();
+    }
+}
