2d60b1aae83430e66285ad61e58033d41f35c31d
jisse Reitsma <jisse@yireo.com>
2023-05-26 08:50:27 +0200

    Copy of PR 148

---
+++ view/frontend/web/js/mixins/step-navigator-mixin.js
@@ -0,0 +1,22 @@
+define([
+    'mage/utils/wrapper',
+    'yireoGoogleTagManagerLogger'
+], function (wrapper, logger, stepNavigator) {
+    'use strict';
+    return function (stepNavigator) {
+        stepNavigator.steps.subscribe(function (steps) {
+            if (steps[0].isVisible()) {
+                const gtmData = window['YIREO_GOOGLETAGMANAGER2_BEGIN_CHECKOUT'];
+
+                if (gtmData === null || gtmData === undefined) {
+                    logger('skipped "begin_checkout" event because data is empty')
+                }
+
+                logger('page event "begin_checkout" (js)', gtmData);
+                window.dataLayer.push({ecommerce: null});
+                window.dataLayer.push(gtmData);
+            }
+        });
+        return stepNavigator;
+    }
+});
