224da4680e820b3456be5e88fe2d07cf5afb8909
Jisse Reitsma <jisse@yireo.com>
2023-08-09 07:13:40 +0200

    Use correct ID reference

--- view/frontend/web/js/product/clicks.js
+++ view/frontend/web/js/product/clicks.js
@@ -6,7 +6,7 @@ define([
         const productPath = config.productPath || '.product-items a.product';
         $(productPath).click(function(event) {
             const debugClicks = window['YIREO_GOOGLETAGMANAGER2_DEBUG_CLICKS'] || false;
-            const $parent = $(this).closest('[id^=product_item_info_]');
+            const $parent = $(this).closest('[id^=product-item-info_]');
             const regex = /_(\d+)$/;
             const matches = $parent.attr('id').match(regex);
             const productId = matches[1];
