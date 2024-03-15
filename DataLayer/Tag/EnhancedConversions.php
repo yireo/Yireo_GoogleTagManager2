f1c7ce6ed2899bf197b92755bc1d11f25ec3fdc7
Jisse Reitsma <jisse@yireo.com>
2024-03-15 12:33:02 +0100

    Compat PHP 7.4

--- DataLayer/Tag/EnhancedConversions.php
+++ DataLayer/Tag/EnhancedConversions.php
@@ -3,6 +3,7 @@
 namespace Yireo\GoogleTagManager2\DataLayer\Tag;
 
 use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Sales\Api\Data\OrderAddressInterface;
 use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Yireo\GoogleTagManager2\Api\Data\MergeTagInterface;
@@ -40,7 +41,10 @@ class EnhancedConversions implements MergeTagInterface
     private function getSha256CustomerTelephone(): string
     {
         $billingAddress = $this->getOrder()->getBillingAddress();
-        return $this->sha256((string)$billingAddress?->getTelephone());
+        if (false === $billingAddress instanceof OrderAddressInterface) {
+            return '';
+        }
+        return $this->sha256((string)$billingAddress->getTelephone());
     }
 
     private function sha256(string $value): string
