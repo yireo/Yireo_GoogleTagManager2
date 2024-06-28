13990fd41caf53acd045887e08a44eda5d3c3505
Jisse Reitsma <jisse@yireo.com>
2024-06-28 13:02:58 +0200

    Remove outdated preinstall

--- .github/workflows/extdn-integration-tests-pre-install.sh
+++ .github/workflows/extdn-integration-tests-pre-install.sh
@@ -7,5 +7,3 @@ composer require yireo/magento2-integration-test-helper --no-update
 composer require yireo/magento2-replace-bundled:^4.0 --no-update
 composer require yireo/magento2-replace-inventory:^4.0 --no-update
 composer require yireo/magento2-replace-pagebuilder:^4.0 --no-update
-
-composer update
