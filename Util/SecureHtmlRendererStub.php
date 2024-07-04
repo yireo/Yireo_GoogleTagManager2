bb56be86424d840ee9eda8c8033510da35d8adb4
Pieter Hoste <pieter@baldwin.be>
2024-07-04 13:46:29 +0200

    Extend from SecureHtmlRenderer\Proxy to delay the instantiation of this class. This fixes the CSP functionality in Magento's production mode.

--- Util/SecureHtmlRendererStub.php
+++ Util/SecureHtmlRendererStub.php
@@ -12,7 +12,7 @@ namespace Yireo\GoogleTagManager2\Util;
 
 // phpcs:ignoreFile
 if (class_exists('\Magento\Framework\View\Helper\SecureHtmlRenderer')) {
-    class SecureHtmlRendererStub extends \Magento\Framework\View\Helper\SecureHtmlRenderer {}
+    class SecureHtmlRendererStub extends \Magento\Framework\View\Helper\SecureHtmlRenderer\Proxy {}
 } else {
     class SecureHtmlRendererStub
     {
