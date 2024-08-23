61e160a08cd814893c03728bb9dd79868518d799
Jisse Reitsma <jisse@yireo.com>
2024-08-23 19:26:27 +0200

    Clean workflows

--- .github/workflows/extdn-integration-tests-pre-install.sh
+++ .github/workflows/extdn-integration-tests-pre-install.sh
@@ -2,7 +2,7 @@
 composer config minimum-stability dev
 composer config prefer-stable false
 
-composer require yireo/magento2-integration-test-helper --no-update
+composer require --dev yireo/magento2-integration-test-helper --no-update
 
 composer require yireo/magento2-replace-bundled:^4.0 --no-update
 composer require yireo/magento2-replace-inventory:^4.0 --no-update
