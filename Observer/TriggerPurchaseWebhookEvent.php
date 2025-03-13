0a7a7c353db1d5c769cabf9210622f1c9d6c3a42
Jeroen Frenken <jeroen@frostup.com>
2025-03-13 20:57:50 +0100

    Bug fixes

--- Observer/TriggerPurchaseWebhookEvent.php
+++ Observer/TriggerPurchaseWebhookEvent.php
@@ -27,9 +27,8 @@ class TriggerPurchaseWebhookEvent implements ObserverInterface
 
     public function execute(Observer $observer)
     {
-        $invoice = $observer->getEvent()->getInvoice();
         /** @var OrderInterface $order */
-        $order = $invoice->getOrder();
+        $order = $observer->getOrder();
 
         $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): has been triggered');
         $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): order_id: ' . $order->getId());
@@ -40,12 +39,6 @@ class TriggerPurchaseWebhookEvent implements ObserverInterface
         $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): order_total_paid: ' . $order->getTotalPaid());
         $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): order_total_due: ' . $order->getTotalDue());
         $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): order_payment_method: ' . $order->getPayment()->getMethod());
-        
-        $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): invoice_id: ' . $invoice->getId());
-        $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): invoice_increment_id: ' . $invoice->getIncrementId());
-        $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): invoice_state: ' . $invoice->getState());
-        $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): invoice_grand_total: ' . $invoice->getGrandTotal());
-        $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): invoice_total_paid: ' . $invoice->getGrandTotal());
         
         $this->debugger->debug('TriggerPurchaseWebhookEvent::execute(): data_has_changed_for_total_paid: ' . 
             ($order->dataHasChangedFor('total_paid') ? 'true' : 'false'));
